b6683bc48187beeaf8596fcf9823950ccc2fc2dc
shioshio0531 <shiori.u0531@gmail.com>
2020-05-05 14:13:58 +0900

    required

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 lock '3.13.0'
-set :application, 'frema-app'
+set :application, 'restaurant-list'
 set :repo_url,  'git@github.com:shioshio0531284/restaurant-list.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/frema-app.pem'] 
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/restaurant-list.pem'] 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
